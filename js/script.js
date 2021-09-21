92f6e58ba96e15146a79e5fecfd807269af1ec46
Rawand Mohammed <rawvsmaw22@gmail.com>
2021-09-21 18:25:38 +0200

    added navbar opacity

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 var className = "inverted";
 var scrollTrigger = 60;
-
 window.onscroll = function() {
   // We add pageYOffset for compatibility with IE.
   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
     document.getElementsByTagName("header")[0].classList.add(className);
-    document.getElementById('nav-bar').style.marginTop = '40px';
   } else {
     document.getElementsByTagName("header")[0].classList.remove(className);
-    document.getElementById('nav-bar').style.marginTop = '40px';
   }
 };
