263f83c2e8ed023ebac1f3c5550d29c74b5577dd
Rawand Mohammed <rawvsmaw22@gmail.com>
2021-09-22 12:01:47 +0200

    site fully done MVG SÄGER KRISTIAN

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-var className = "inverted";
-var scrollTrigger = 60;
-window.onscroll = function() {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    document.getElementsByTagName("header")[0].classList.add(className);
-  } else {
-    document.getElementsByTagName("header")[0].classList.remove(className);
-  }
-};
+const header = document.querySelector(".nav-bar");  //classnamnet (eller id-namnet beroende av vad ni satt i html:en) på er navbar
+const target = document.querySelector("#contact-section"); //classnamnet (eller id-namnet beroende av vad ni satt i html:en) på det element som kommer trigga style-ändringen när man scrollar till det
+
+window.addEventListener("load", function () {
+  document.addEventListener("scroll", () => {
+    if (window.scrollY >= target.getBoundingClientRect().top) {
+      header.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+      header.style.marginTop = "0px";
+    } else {
+      header.style.backgroundColor = "transparent";
+      header.style.marginTop = "30px";
+    }
+  });
+})
+// Toggle menu script here
+var menuList = document.getElementById("menuList");
+            menuList.style.maxWidth = "0px";
+            function togglemenu(){
+
+                if(menuList.style.maxWidth == "0px"){
+                    menuList.style.maxWidth = "150px"
+                    
+                }
+                else{
+                    menuList.style.maxWidth = "0px"
+                   
+                }
+
+            }
